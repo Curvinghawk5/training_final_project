f4f5a9092896fb3239695b278c147718fa6194a0
Curvinghawk5 <72762608+Curvinghawk5@users.noreply.github.com>
2025-08-27 11:32:17 +0100

    Backend API integration began

--- backend/src/controllers/controllers.js
+++ backend/src/controllers/controllers.js
@@ -1,10 +1,30 @@
 const YahooFinance = require("yahoo-finance2").default;
 
-async function getCurrentPrice(tag) {
-    const result = await YahooFinance.quote(`${tag}`);
-    console.log(result.regularMarketPrice, result.currency);
+async function getCurrentPrice(req, res) {
+    const {tag} = req.params;
+    const result = await YahooFinance.quote(tag);
+    res.status(200).json({ask: result.ask, bid: result.bid, currency: result.currency});
+}
+
+async function searchForCompany(req, res) {
+    const search = req.params.query;
+    const result = await YahooFinance.search(search);
+    res.status(200).json(result.quotes);
+}
+
+async function searchNews(req, res) {
+    const search = req.params.query;
+    const result = await YahooFinance.search(search);
+    res.status(200).json(result.news);
+}
+
+async function buyStocks(req, res) {
+    
 }
 
 module.exports = {
-    getCurrentPrice
+    getCurrentPrice,
+    searchForCompany,
+    searchNews,
+    buyStocks
 }
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controllers");
 
-router.get("/price", controller.getCurrentPrice);
+router.get("/price/:tag", controller.getCurrentPrice);
+router.get("/search/:query", controller.searchForCompany);
+router.get("/search/news/:query", controller.searchNews);
+router.post("/buy/:tag", controller.buyStocks);
 
 module.exports = router;
